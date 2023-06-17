6775c4c189a1771ddfe88037a9cf4da126d43d27
Gokhan <gokhangndz@gmail.com>
2023-06-17 15:17:07 +0300

    sql client connect with dapper

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +20,14 @@ namespace TrackerLibrary.DataAccess
         {
             //throw new NotImplementedException();
             
-            model.Id = 1;
+            //model.Id = 1;
             
-            return model;
+            //return model;
+
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(GlobalConfig.CnnString("Tournaments"))
+            {
+
+            }
         }
     }
 }
